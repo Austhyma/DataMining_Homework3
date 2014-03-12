9372f79438def661a268b0f422429fbccd065dfa
Austhyma <austhyma@gmail.com>
2014-03-12 18:31:07 -0500

    Latest Changes

---
+++ Distance.java
@@ -0,0 +1,12 @@
+/*
+ * File: Distance.java
+ */
+
+public class Distance {
+  
+  private double euclidean;
+  private double chebyshev;
+  private double manhattan;
+  private double cosine;
+  
+}
--- KMeansClustering.java
+++ KMeansClustering.java
@@ -61,11 +61,18 @@ public class KMeansClustering {
   }
   
   public static void main(String[] args) throws IOException {
+    
+    //Reads/processes training/testing files
     ArrayList<Data> trainingData = processData(args[0]);
     if (trainingData == null) {System.out.println("Invalid training file."); return;}
     ArrayList<Data> testingData = processData(args[1]);
     if (testingData == null) {System.out.println("Invalid testing file."); return;}
     
-    System.out.println(trainingData.get(0).getClassLabel());
+    ArrayList<Data> allData = new ArrayList<Data>(trainingData);
+    System.out.println("Initial " + allData.size());
+    allData.addAll(testingData);
+    System.out.println("Final " + allData.size());
+    System.out.println("Actual " + (trainingData.size() + testingData.size()));
+    
   }
 }
